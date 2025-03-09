ed583289bbf7cd8ee478e509d56872f5e7ae5894
thecorf <mim010@edu.zealand.dk>
2025-03-09 22:57:19 +0100

    Add files via upload

---
+++ Bluebook/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluebook
+{
+    internal class Person
+    {
+		private int _age;
+
+		public int Age
+		{
+			get { return _age; }
+			set { _age = value; }
+		}
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		private string _class;
+
+		public string Class
+		{
+			get { return _class; }
+			set { _class = value; }
+		}
+		private string _birthdate;
+
+		public string Birthdate
+		{
+			get { return _birthdate; }
+			set { _birthdate = value; }
+		}
+
+
+		public override string ToString()
+        {
+            return $"age {Age}\nname {Name}\nclass {Class}\nbirthdate {Birthdate}";
+        }
+
+    }
+}
---
+++ Bluebook/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Bluebook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Person person1 = new Person();
+            Person person2 = new Person();
+            Person person3 = new Person();
+            person.Age =29;
+            person.Name = "Michael";
+            person.Class = "1.C";
+            person.Birthdate = "21.Sep";
+
+            person1.Age = 21;
+            person1.Name = "Marcus";
+            person1.Class = "1.C";
+            person1.Birthdate = "3. Feb";
+
+            person2.Age = 32;
+            person2.Name = "Kurt";
+            person2.Class = "4.C";
+            person2.Birthdate = "5. Maj";
+
+            person3.Age = 74;
+            person3.Name = "Linus";
+            person3.Class = "1.C";
+            person3.Birthdate = "9. Dec";
+            Console.WriteLine(person);
+            Console.WriteLine("----------------");
+            Console.WriteLine(person1);
+            Console.WriteLine("----------------");
+            Console.WriteLine(person2);
+            Console.WriteLine("----------------");
+            Console.WriteLine(person3);
+        }
+    }
+}
